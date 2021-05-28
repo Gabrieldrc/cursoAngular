a9cd1a8b8cb2069b7ae06deddc80e8de47e9231f
Gabriel <you@example.com>
2021-05-28 20:04:50 -0300

    Se agrego image a la interfaz y se utilizo en el componente para asegurar que la lista tiene que cumplir la interfaz de Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.models';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platzi-store';
   items = ['gabriel', 'gabriela', 'gabrielle']
-  products = [
+  products: Product[] = [
     {
       id: '1',
       image: 'assets/images/camiseta.png',
