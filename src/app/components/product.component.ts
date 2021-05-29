ded522cc047c5dfb1e0d91d83c1961915f79e037
Gabriel <you@example.com>
2021-05-28 21:16:39 -0300

    Agregamos un input para que a travez de el atributo [product] puedan mandarle el valor de la propiedad product. Nota: Se mostraba un error indicando que la variable product no se habia inicializado, eso se corrigio al cambiar el strict de la configuracion de tscript a false

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../product.models';
 @Component({
   selector: 'app-product',
   templateUrl: './producto.component.html',
 })
 export class ProductComponent {
-  product: Product = {
-    id: '1',
-    image: 'assets/images/camiseta.png',
-    title: 'Camiseta',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  };
+  @Input() product: Product;
+  addCart() {
+    console.log("AÑADIR AL CARRITO")
+  }
 }
