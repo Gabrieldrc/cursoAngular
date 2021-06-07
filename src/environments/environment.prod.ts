fee318f2d63a714b817a879a3fe859d67b0c5fb1
Gabriel <you@example.com>
2021-06-07 09:59:21 -0300

    Se agrego la libreria de firebase, ademas de actualizar el archivo enviroment con la llave de firebase. Ademas se agregaron los modulos necesarios de firebase como auth y storage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   url_api: 'https://platzi-store.herokuapp.com/',
+  firebase: {
+    apiKey: 'AIzaSyB9zr3ssAPcclFapCTNwiMLUXwpMGiqqF0',
+    authDomain: 'platzi-store-674ff.firebaseapp.com',
+    projectId: 'platzi-store-674ff',
+    storageBucket: 'platzi-store-674ff.appspot.com',
+    messagingSenderId: '580894955103',
+    appId: '1:580894955103:web:18c89a6339c83af5e22dea'
+  },
 };
