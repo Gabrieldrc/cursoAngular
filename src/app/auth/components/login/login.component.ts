a188edbf12449d6e91ca05f2e2d4747c070281e0
Gabriel <you@example.com>
2021-06-07 11:18:25 -0300

    Se agrego el formulario de login con el servicio de auth de login. Ademas se agrego el boton de logout en el componente nav de admin

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      this.authService.login(value.email, value.password)
+        .then(() => {
+          this.router.navigate(['/admin']);
+        })
+        .catch(error => {
+          alert('no es valido el email y/o contraseña')
+        });
+    }
   }
 
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -12,4 +12,10 @@ export class AuthService {
   createUser(email: string, password: string) {
     return this.af.createUserWithEmailAndPassword(email, password);
   }
+  login(email: string, password: string) {
+    return this.af.signInWithEmailAndPassword(email, password);
+  }
+  logout() {
+    return this.af.signOut();
+  }
 }
