ec8d16ecd7a224b2784e032f4bf0aff5c48fb710
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-25 17:06:14 -0300

    Se agrego un pipe para realizar el fetch y simplemente recibir el producto y evitar doble subscribe en el codigo y que sea mas optimo

--- src/app/products/components/product-detail/product-detail.component.ts
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '@core/services/product/products.service';
 import { Product } from '@core/models/product.models';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,21 +11,18 @@ import { Product } from '@core/models/product.models';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  product: Product;
+  product$: Observable<Product>;
   constructor(
     private route: ActivatedRoute,
     private productService: ProductsService,
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.fetchProduct(params.id)
-    });
-  }
-  fetchProduct(id: string) {
-    this.productService.getProduct(id)
-      .subscribe(product => {
-        this.product = product;
-      });
+    this.product$ = this.route.params
+    .pipe(
+      switchMap((params: Params) => {
+        return this.productService.getProduct(params.id)
+      })
+    );
   }
 }
